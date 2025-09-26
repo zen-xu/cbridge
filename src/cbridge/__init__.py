6465da643f0d20f676016eefa2d5ee78da7e667a
ZhengYu, Xu <zen-xu@outlook.com>
2025-09-26 21:44:00 +0800

    feat: Refactor field handling and add default value support (#5)
    * feat: Refactor field handling and add default value support
    
    This commit introduces a significant refactoring of the `CStruct` field handling mechanism and adds support for default values.
    
    The key changes are:
    
    - **Improved Array Handling:** The custom `types.Array` implementation has been replaced with a more modern approach using `__class_getitem__`. This allows for a more intuitive and standard way of defining array fields (e.g., `Array[types.int, 4]`)
    
    - **Default Value Support:** `CStruct` fields now support default values, including `default` and `default_factory` from `dataclasses.field`. This makes creating `CStruct` instances more flexible.
    
    - **Refactored Initialization:** The `__init__` method of `CStruct` has been rewritten to be more robust. It now correctly handles a mix of positional and keyword arguments, and properly applies default values and initializes array fields.
    
    * update tests

--- src/cbridge/__init__.py
+++ src/cbridge/__init__.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 from .cbridge import CStruct
+from .cbridge import field
 
 
 __version__ = "0.1.0b0"
 __authors__ = [
     "ZhengYu, Xu <zen-xu@outlook.com>",
 ]
-__all__ = ["CStruct"]
+__all__ = ["CStruct", "field"]
--- tests/test_cstruct.py
+++ tests/test_cstruct.py
@@ -1,8 +1,10 @@
 import array
 
+from typing import ClassVar
 from typing import Literal
 
 from cbridge import CStruct
+from cbridge import field
 from cbridge import types
 
 
@@ -64,11 +66,39 @@ def test_repr_cstruct():
 class ArrayData(CStruct):
     a: types.int
     b: types.Array[types.int8, Literal[2]]
+    c: types.Array[types.int8, 2]  # type: ignore[type-var]
 
 
 def test_array_cstruct():
-    data = ArrayData(a=1, b=(1, 2))
+    data = ArrayData(1, b=(1, 2), c=(3, 4))
     assert data.a == 1
     assert data.b == [1, 2]
     assert data.b != [2, 3]
-    assert repr(data) == "ArrayData(a=1, b=[1, 2])"
+    assert data.c == [3, 4]
+    assert repr(data) == "ArrayData(a=1, b=[1, 2], c=[3, 4])"
+
+
+class DefaultData(CStruct):
+    a: types.int
+    b: types.int = 1
+    c: types.int = field(default=2)
+    d: types.Array[types.int, Literal[2]] = field(default_factory=lambda: [3, 4])
+
+
+def test_default_cstruct():
+    data = DefaultData(a=0)
+    assert data.a == 0
+    assert data.b == 1
+    assert data.c == 2
+    assert data.d == [3, 4]
+
+
+class ClassVarData(CStruct):
+    a: ClassVar[int]
+    b: types.int
+
+
+def test_class_var_cstruct():
+    data = ClassVarData(b=1)
+    assert data.b == 1
+    assert str(data) == "ClassVarData(b=1)"
